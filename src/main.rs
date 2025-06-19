8aa852b344a37f3bdeeed1d2f1d344ef00584a11
Mateusz Kurzyna <lolc44t@gmail.com>
2025-06-19 20:29:51 +0200

    add headers list

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Read},
+    io::{BufRead, BufReader},
 };
 
 use dotenv::dotenv;
 use walkdir::{DirEntry, WalkDir};
 
+// TODO: Add path as a second value
 #[derive(Debug)]
 struct Topics {
     h1: Vec<String>,
@@ -15,16 +16,31 @@ struct Topics {
 
 impl Topics {
     fn new(files: Vec<DirEntry>) -> Topics {
-        let h1: Vec<String> = vec![];
-        let h2: Vec<String> = vec![];
-        let h3: Vec<String> = vec![];
+        let mut h1: Vec<String> = vec![];
+        let mut h2: Vec<String> = vec![];
+        let mut h3: Vec<String> = vec![];
 
         for f in files {
             match File::open(f.path()) {
                 Ok(f) => {
                     let lines = BufReader::new(f).lines();
 
-                    for line in lines.map_while(Result::ok) {}
+                    for line in lines.map_while(Result::ok) {
+                        let sl: Vec<&str> = line.splitn(2, ' ').collect();
+                        // TODO: Add header sanetization
+                        match sl[0] {
+                            "#" => {
+                                h1.push(sl[1].to_string());
+                            }
+                            "##" => {
+                                h2.push(sl[1].to_string());
+                            }
+                            "###" => {
+                                h3.push(sl[1].to_string());
+                            }
+                            _ => {}
+                        }
+                    }
                 }
                 Err(e) => {
                     eprintln!("Couldn't open file {e}")
@@ -42,14 +58,19 @@ fn main() {
     let dir_path = std::env::var("DIR_PATH").expect("DIR_PATH is not set");
     let file_path = std::env::var("FILE_PATH").expect("FILE_PATH is not set");
 
+    // TODO: remove reference to itself(file)
     let files_in_dir: Vec<DirEntry> = WalkDir::new(dir_path)
         .max_depth(3)
         .into_iter()
         .filter_map(Result::ok)
-        .filter(|e| e.path().extension() == Some("md".as_ref()))
+        .filter(|e| {
+            e.path().extension() == Some("md".as_ref())
+                && e.path().to_str() != Some(file_path.as_str())
+        })
         .collect();
 
     println!("=== FILES ===");
+
     for (i, path) in files_in_dir.iter().enumerate() {
         println!("{}. {}", i, path.file_name().display());
     }
@@ -60,6 +81,8 @@ fn main() {
 
             let topics = Topics::new(files_in_dir);
 
+            println!("=== TOPICS ===");
+            println!("{:?}", topics);
             println!("=== FILE CONTENT ===");
             for line in lines.map_while(Result::ok) {
                 println!("{}", line);
